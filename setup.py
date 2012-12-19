3dc33f787391eccc57e21e98dc4c04331a359c8e
Andrey Antukh <niwi@niwi.be>
2012-12-19 23:45:17 +0100

    Add LICENSE and setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+description="""
+Improved and complement gettext support for javascript for Django
+"""
+
+setup(
+    name = "django-jsgettext",
+    version = '0.4.1',
+    url = 'https://github.com/niwibe/django-jsgettext',
+    license = 'BSD',
+    platforms = ['OS Independent'],
+    description = description.strip(),
+    author = 'Andrey Antukh',
+    author_email = 'niwi@niwi.be',
+    maintainer = 'Andrey Antukh',
+    maintainer_email = 'niwi@niwi.be',
+    packages = find_packages(),
+    include_package_data = False,
+    install_requires = [],
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
